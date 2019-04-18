f829ac94bd71949c924d2816105ef4fac7a0b424
core.editor <andreeavillatte@gmail.com>
2019-04-18 11:59:26 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //si je clique sur le bouton
+    $('button').click(function () {
+    // je cache les lignes qui ont la classe hide
+        $('.hide').hide();
+    });
+});
